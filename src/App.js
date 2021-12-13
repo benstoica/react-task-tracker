088b3f99a476816dd649af421be2eeeb9b342151
Ben Stoica <bstoica@uwm.com>
2021-12-13 15:19:30 -0500

    button toggling form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -47,9 +48,12 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <Header title='Task Tracker' />
-      <AddTask onAdd={addTask} />
+    <div className='container'>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
